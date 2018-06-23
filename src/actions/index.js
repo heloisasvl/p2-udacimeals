d59edc9e7acb0a6951d21c7ccb1733aae1d50f79
Heloisa Araújo <heloisasvl@gmail.com>
2018-06-23 11:42:39 -0300

    feat: Adiciona formatação pretty

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,19 +1,19 @@
-export const ADD_RECIPE = 'ADD_RECIPE'
-export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR'
+export const ADD_RECIPE = 'ADD_RECIPE';
+export const REMOVE_FROM_CALENDAR = 'REMOVE_FROM_CALENDAR';
 
-export function addRecipe ({ day, recipe, meal }) {
+export function addRecipe({ day, recipe, meal }) {
   return {
     type: ADD_RECIPE,
     day,
     recipe,
     meal
-  }
+  };
 }
 
-export function RemoveFromCalendar ({ day, meal }) {
+export function RemoveFromCalendar({ day, meal }) {
   return {
     type: REMOVE_FROM_CALENDAR,
     day,
     meal
-  }
+  };
 }
